c46cba3a21747a3be0e66ff7e7bdcc75355376d1
Manuel <benitessolismanuel@gmail.com>
2023-02-04 18:43:25 -0500

    Updates

--- src/component/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ content }) => {
-	return (
-		<button type="submit" className="btn-style">
-			{content}
-		</button>
-	)
-}
-
-export default Button
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import valuesReducer from "../store/valuesSlice"
-export const store = configureStore({
-	reducer: {
-		values: valuesReducer,
-	},
-})
